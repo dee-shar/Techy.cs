3d07b09f096c2f5eac006f219ba91ff29ba7741f
huok <77536370+dee-shar@users.noreply.github.com>
2025-11-08 18:52:37 +0300

    Create Techy.cs

---
+++ src/Techy.cs
@@ -0,0 +1,23 @@
+using System.Net.Http;
+
+namespace TechyApi
+{
+    public class Techy
+    {
+        private readonly HttpClient httpClient;
+        private readonly string apiUrl = "https://techy-api.vercel.app/api";
+        public Techy()
+        {
+            httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.UserAgent.ParseAdd(
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36"
+            );
+        }
+
+        public async Task<string> GetPhrase()
+        {
+            var response = await httpClient.GetAsync($"{apiUrl}/api/json");
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
